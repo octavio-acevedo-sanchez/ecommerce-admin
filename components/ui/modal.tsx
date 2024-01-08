fb6ac3b9ce91de23619e23396560a381f11076b0
Octavio Acevedo Sanchez <octavio.acevedo.sanchez@gmail.com>
2024-01-08 17:42:33 -0500

    Added Modal and Form components

---
+++ components/ui/modal.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle
+} from '@/components/ui/dialog';
+
+interface ModalProps {
+	title: string;
+	description: string;
+	isOpen: boolean;
+	onClose: () => void;
+	children?: React.ReactNode;
+}
+
+export const Modal = ({
+	title,
+	description,
+	isOpen,
+	onClose,
+	children
+}: ModalProps): React.ReactNode => {
+	const onChange = (open: boolean): void => {
+		if (!isOpen) {
+			onClose();
+		}
+	};
+
+	return (
+		<Dialog open={isOpen} onOpenChange={onChange}>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>{title}</DialogTitle>
+					<DialogDescription>{description}</DialogDescription>
+				</DialogHeader>
+				<div>{children}</div>
+			</DialogContent>
+		</Dialog>
+	);
+};
